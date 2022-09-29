08e5ec97ec530c0068845ed5b5fc4f8117d5d1d0
paul <105162443+pl8nt@users.noreply.github.com>
2022-09-29 14:16:51 -0500

    fixed namespace issue

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,80 +1,81 @@
-﻿namespace Lab2;
-public partial class MainPage : ContentPage
+﻿namespace Lab2
 {
-	public MainPage()
-	{
-		InitializeComponent();
-		EntriesLV.ItemsSource = MauiProgram.bl.GetEntries();
-	}
-
-
-    /// <summary>
-    /// This is the AddEntry button event that will take the user input 
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-	void AddEntry(System.Object sender, System.EventArgs e)
-	{
-        if (int.TryParse(Difficulty.Text, out int difficulty))      
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
         {
-            var message = MauiProgram.bl.AddEntry(Clue.Text, Answer.Text, difficulty, Date.Text);
-            if (message != InvalidFieldError.NoError)
+            InitializeComponent();
+            EntriesLV.ItemsSource = MauiProgram.bl.GetEntries();
+        }
+
+
+        /// <summary>
+        /// This is the AddEntry button event that will take the user input 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void AddEntry(System.Object sender, System.EventArgs e)
+        {
+            if (int.TryParse(Difficulty.Text, out int difficulty))
             {
-                DisplayAlert("Add Entry Message:", message.ToString(), "Ok");
+                var message = MauiProgram.bl.AddEntry(Clue.Text, Answer.Text, difficulty, Date.Text);
+                if (message != InvalidFieldError.NoError)
+                {
+                    DisplayAlert("Add Entry Message:", message.ToString(), "Ok");
+                }
+            }
+            else
+            {
+                DisplayAlert("Add Entry Message:", "InvalidDifficulty", "Ok");
             }
         }
-        else
+
+
+        /// <summary>
+        /// This is the DeleteEntry button that will find the SelectedItem in the listview and return the entry that is selected to be deleted
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void DeleteEntry(System.Object sender, System.EventArgs e)
         {
-            DisplayAlert("Add Entry Message:", "InvalidDifficulty", "Ok");
-        }
-    }
+            Entry entry = (Entry)EntriesLV.SelectedItem;
 
-
-	/// <summary>
-    /// This is the DeleteEntry button that will find the SelectedItem in the listview and return the entry that is selected to be deleted
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-	void DeleteEntry(System.Object sender, System.EventArgs e)
-	{
-		Entry entry = (Entry)EntriesLV.SelectedItem;
-
-        if(entry != null)
-        {
-            var message = MauiProgram.bl.DeleteEntry(entry);
-            if (message != EntryDeletionError.NoError)
+            if (entry != null)
             {
-                DisplayAlert("Delete Entry Message:", message.ToString(), "Ok");
+                var message = MauiProgram.bl.DeleteEntry(entry);
+                if (message != EntryDeletionError.NoError)
+                {
+                    DisplayAlert("Delete Entry Message:", message.ToString(), "Ok");
+                }
+            }
+            else
+            {
+                DisplayAlert("Delete Entry Message:", "EntryNotFound", "Ok");
             }
         }
-        else
+
+
+        /// <summary>
+        /// This is the EditEntry button that will find the entry that was selected in the listview and change its input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void EditEntry(System.Object sender, System.EventArgs e)
         {
-            DisplayAlert("Delete Entry Message:", "EntryNotFound", "Ok");
-        }
-    }
+            Entry entry = (Entry)EntriesLV.SelectedItem;
 
-
-    /// <summary>
-    /// This is the EditEntry button that will find the entry that was selected in the listview and change its input
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-	void EditEntry(System.Object sender, System.EventArgs e)
-	{
-        Entry entry = (Entry)EntriesLV.SelectedItem;
-
-        if(entry != null)
-        {
-            var message = MauiProgram.bl.EditEntry(entry, Clue.Text, Answer.Text, Int32.Parse(Difficulty.Text), Date.Text, entry.Id);
-            if (message != EntryEditError.NoError)
+            if (entry != null)
             {
-                DisplayAlert("Edit Entry Message:", message.ToString(), "Ok");
+                var message = MauiProgram.bl.EditEntry(entry, Clue.Text, Answer.Text, Int32.Parse(Difficulty.Text), Date.Text, entry.Id);
+                if (message != EntryEditError.NoError)
+                {
+                    DisplayAlert("Edit Entry Message:", message.ToString(), "Ok");
+                }
             }
-        }
-        else
-        {
-            DisplayAlert("Edit Entry Message:", "EntryNotFound", "Ok");
+            else
+            {
+                DisplayAlert("Edit Entry Message:", "EntryNotFound", "Ok");
+            }
         }
     }
 }
-
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -9,24 +9,25 @@
 *				helped me learn the material quicker and also develops our skills in working in teams.
 */
 
-namespace Lab2;
-
-public static class MauiProgram
+namespace Lab2
 {
-	// the business logic that will send and retrieve data to the database
-    public static IBusinessLogic bl = new BusinessLogic();
+    public static class MauiProgram
+    {
+        // the business logic that will send and retrieve data to the database
+        public static IBusinessLogic bl = new BusinessLogic();
 
-    public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-		return builder.Build();
-	}
+            return builder.Build();
+        }
+    }
 }
